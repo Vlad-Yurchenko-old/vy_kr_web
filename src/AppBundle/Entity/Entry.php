d07d654f9d8da808b21d8d3a89f149b673cfac14
Vlad-Yurchenko <slyganprolol@gmail.com>
2017-05-10 12:19:53 +0300

    first

---
+++ src/AppBundle/Entity/Entry.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Entry
+ *
+ * @ORM\Table(name="entry")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\EntryRepository")
+ */
+class Entry
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="text", type="string", length=255)
+     */
+    private $text;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="category", type="string", length=255)
+     */
+    private $category;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="time", type="datetime")
+     */
+    private $time;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="worker_id", type="integer")
+     */
+    private $workerId;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set text
+     *
+     * @param string $text
+     *
+     * @return Entry
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    /**
+     * Get text
+     *
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set category
+     *
+     * @param string $category
+     *
+     * @return Entry
+     */
+    public function setCategory($category)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return string
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * Set time
+     *
+     * @param \DateTime $time
+     *
+     * @return Entry
+     */
+    public function setTime($time)
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    /**
+     * Get time
+     *
+     * @return \DateTime
+     */
+    public function getTime()
+    {
+        return $this->time;
+    }
+
+    /**
+     * Set workerId
+     *
+     * @param integer $workerId
+     *
+     * @return Entry
+     */
+    public function setWorkerId($workerId)
+    {
+        $this->workerId = $workerId;
+
+        return $this;
+    }
+
+    /**
+     * Get workerId
+     *
+     * @return int
+     */
+    public function getWorkerId()
+    {
+        return $this->workerId;
+    }
+}
+
